3c9371847c966077e53388838c97eab4a3cd160a
giuliapapa <73584319+giuliapapa@users.noreply.github.com>
2022-12-09 19:24:07 +0100

    Inserita pagina donazione punti

---
+++ app/src/main/java/com/example/shareshelf/PointsDonation.java
@@ -0,0 +1,14 @@
+package com.example.shareshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PointsDonation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points_donation);
+    }
+}
