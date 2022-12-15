bcb12d12131fd595157edf8f7051c6853e0f9368
giuliapapa <73584319+giuliapapa@users.noreply.github.com>
2022-12-15 13:46:23 +0100

    Aggiunta attività "modifica profilo"

---
+++ app/src/main/java/com/example/shareshelf/ModifyProfile.java
@@ -0,0 +1,14 @@
+package com.example.shareshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profile);
+    }
+}
