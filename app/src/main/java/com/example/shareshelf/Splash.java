88bb19d10a12f8d3f13dc27c4adac614e1895789
giuliapapa <73584319+giuliapapa@users.noreply.github.com>
2023-01-15 15:07:53 +0100

    implementato splash screen

---
+++ app/src/main/java/com/example/shareshelf/Splash.java
@@ -0,0 +1,33 @@
+package com.example.shareshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //forse da eliminare riga 18
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+        //VEDI MANIFEST
+        //scambia Splash con Main
+
+    }
+}
